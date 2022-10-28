5a25ccb35c8044fc34ede08fd6eb78ec7bdce773
timmySpark <timmyspark1@gmail.com>
2022-10-28 19:25:56 +0100

    updated settings.py content

--- stageone/settings.py
+++ stageone/settings.py
@@ -137,16 +137,6 @@ STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ALLOW_ALL_ORIGINS = False
+CORS_ALLOW_ALL_ORIGINS = True
 
-CORS_ALLOWED_ORIGINS = [
-    "https://hng-stageone.herokuapp.com",
-    "https://hng-stageone.herokuapp.com/api/biodata",
-    "http://localhost:8000",
-    
-]
-CORS_ALLOW_METHODS = [
-    "GET",
-    "OPTIONS",
-]
 django_heroku.settings(locals())
